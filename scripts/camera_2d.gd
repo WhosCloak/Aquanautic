37fc470a865d9088f07834a140af04752860958d
Jeremy-Loz <lozanoj2790@gmail.com>
2025-09-07 21:19:55 -0500

    Inhereted Scenes and Levels
    asdsdac

--- scripts/camera_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Camera2D
-#the following code prevents the player from seeing past the ocean floor#
-@export var floor_y: float = 830.0
-@export var padding: float = 0.0
-
-func _process(_delta: float) -> void:
-	var half_h := get_viewport_rect().size.y * 0.5 / zoom.y
-	var  max_cam_y := floor_y - half_h + padding
-	global_position.y = min(global_position.y, max_cam_y)
--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	$AudioStreamPlayer2D.play()
 	
 func _on_button_pressed() -> void: #START
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/Base_Level/Level1.tscn")
 
 func _on_button_2_pressed() -> void: #OPTIONS
 	pass # Replace with settings
