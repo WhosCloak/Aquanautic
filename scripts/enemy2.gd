819734fd7ae73d43b4eb960c867a630116d0c171
Jeremy-Loz <lozanoj2790@gmail.com>
2025-10-03 20:37:53 -0500

    Eels Shield

--- scripts/enemy2.gd
+++ scripts/enemy2.gd
@@ -5,7 +5,9 @@ var speed = 150
 var player = Node2D
 var deathsound = preload("res://audios/enemydeath.wav")
 @onready var art:AnimatedSprite2D = $Sprite2D
+@onready var shield_sprite: AnimatedSprite2D = $ShieldSprite
 var flip_threshold := 1.0
+var shield_active := true
 
 #Model Flip
 func _update_art_facing() -> void:
@@ -18,6 +20,7 @@ func _update_art_facing() -> void:
 #Find Player
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
+	shield_sprite.play("shield_activate")
 
 func _physics_process(_delta):
 	if player:
@@ -31,6 +34,13 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if body.has_method("take_damage"):
 			body.take_damage(1)
+		die()
+
+func take_damage():
+	if shield_active:
+		shield_active = false
+		shield_sprite.visible = false
+	else:
 		die()
 
 #Enemy Death
--- scripts/harpoon.gd
+++ scripts/harpoon.gd
@@ -6,10 +6,12 @@ func _ready():
    
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("enemy"):
-		if body.has_method("die"):
+		if body.is_in_group("shielded_enemy") and body.has_method("take_damage"):
+			body.take_damage()
+		elif body.has_method("die"):
 			body.die()
 		queue_free()
 	elif body.is_in_group("boss"):
 		if body.has_method("apply_damage"):
 			body.apply_damage(1)
-		queue_free()
+			queue_free()
