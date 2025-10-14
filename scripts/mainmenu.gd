a872ebbcf1f3178cb8d6af528714647568cf998e
Jeremy-Loz <lozanoj2790@gmail.com>
2025-10-14 11:54:10 -0500

    Options Menu Expiremental

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,10 +1,16 @@
 extends Control
 
+@onready var mainbuttons: VBoxContainer = $Mainbuttons
+@onready var options: Panel = $Options
+
 var mainmenutheme = preload("res://audios/Into the Abyss(Main Menu).mp3")
 var buttonpress = preload("res://audios/menu_select.wav")
 
 func _ready() -> void:
 	$Mainmenutheme.play()
+	mainbuttons.visible = true
+	options.visible = false
+	
 	
 func _on_button_pressed() -> void: #START
 	Fade.transition()
@@ -13,9 +19,14 @@ func _on_button_pressed() -> void: #START
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_button_2_pressed() -> void: #OPTIONS
-	$buttonpress.play()
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	mainbuttons.visible = false
+	options.visible = true
+	
 
 func _on_button_3_pressed() -> void: #QUIT
 	$buttonpress.play()
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
