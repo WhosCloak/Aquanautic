37e5169b50d10af21db335506180fcd9d3d20c03
Yalin Tiringa <144388865+WhosCloak@users.noreply.github.com>
2025-10-25 12:42:38 -0500

    Brightness setting

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,10 +3,9 @@ extends Node
 #Global Variables
 var player_score: int = 0
 var high_score: int = 0
-var difficulty = 0
 var randomstrength = 10
 
 func reset():
 	player_score = 0
 	high_score = 0
-	difficulty = 0
+	randomstrength = 10
